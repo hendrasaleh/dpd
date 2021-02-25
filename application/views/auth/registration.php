f432604de0a21bc05762b7d04a98c1362f933869
hendrasaleh <hendrasaleh@gmail.com>
2021-02-25 21:49:59 +0700

    Registration

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -2,29 +2,51 @@
 
 <div class="register-box">
   <div class="register-logo">
-    <b>Admin</b>LTE
+    <b>PKS</b> Kuningan
   </div>
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">Silakan isi form berikut untuk registrasi</p>
 
       <form action="<?= base_url('auth/registration'); ?>" method="post">
         <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
         <div class="input-group mb-3">
-          <input type="text" class="form-control" id="name" name="name" placeholder="Full name" value="<?= set_value('name'); ?>">
+          <input type="text" class="form-control" id="name" name="name" placeholder="Nama lengkap" value="<?= set_value('name'); ?>" required>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
+        <div class="input-group mb-3">
+          <div class="col-6">
+            <td>
+              <div class="icheck-primary d-inline">
+                <input type="radio" id="jenis_kelamin1" name="jenis_kelamin" value="1" required>
+                <label for="jenis_kelamin1">
+                  Laki-laki
+                </label>
+              </div>
+            </td>
+          </div>
+          <div class="col-6">
+            <td>
+              <div class="icheck-success d-inline">
+                <input type="radio" id="jenis_kelamin2" name="jenis_kelamin" value="0" required>
+                <label for="jenis_kelamin2">
+                  Perempuan
+                </label>
+              </div>
+            </td>
+          </div>
+        </div>
         <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
         <div class="input-group mb-3">
-          <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
+          <input type="text" class="form-control" id="email" name="email" placeholder="No handphone (WA)" value="<?= set_value('email'); ?>" required>
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+              <span class="fas fa-phone"></span>
             </div>
           </div>
         </div>
