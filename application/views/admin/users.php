0765eeefb55e9843ec8adfdb63e4da4c2f59f1a0
Hendra Saleh <hendrasaleh@gmail.com>
2021-04-15 09:07:55 +0700

    admin/users

--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -28,40 +28,40 @@
 		  		<?= $this->session->flashdata('message'); ?>
 
 		  		<table id="example1" class="table table-hover">
-				  <thead>
-				    <tr>
-				      <th scope="col">#</th>
-				      <th scope="col">Email</th>
-				      <th scope="col">Name</th>
-				      <th scope="col">Role</th>
-				      <th scope="col">Active</th>
-				      <th scope="col">Date Modified</th>
-				      <th scope="col">Action</th>
-				    </tr>
-				  </thead>
-				  <tbody>
-				  	<?php
-				  		$i = 1;
-				  		foreach ($users as $user) :
-				  	?>
-				    <tr>
-				      <th scope="row"><?= $i; ?></th>
-				      <td><?= $user['email']; ?></td>
-				      <td><?= $user['name']; ?></td>
-				      <td><?= $user['role']; ?></td>
-				      <td><?= $user['is_active'] == 1 ? 'Yes' : 'No'; ?></td>
-				      <td><?= date('d-M-Y H:m:s', $user['date_modified']);?></td>
-				      <td>
-				      		<a href="manageuser/<?= $user['id']; ?>" class="badge badge-success">edit</a>
-				      		<a href="javascript:hapusData(<?= $user['id']; ?>)" class="badge badge-danger">delete</a>
-				      </td>
-				    </tr>
-					<?php
-						$i++;
-						endforeach;
-					?>
-				  </tbody>
-				</table>
+  				  <thead>
+  				    <tr>
+  				      <th scope="col">#</th>
+  				      <th scope="col">Email</th>
+  				      <th scope="col">Name</th>
+  				      <th scope="col">Role</th>
+  				      <th scope="col">Active</th>
+  				      <th scope="col">Date Modified</th>
+  				      <th scope="col">Action</th>
+  				    </tr>
+  				  </thead>
+  				  <tbody>
+  				  	<?php
+  				  		$i = 1;
+  				  		foreach ($users as $user) :
+  				  	?>
+  				    <tr>
+  				      <th scope="row"><?= $i; ?></th>
+  				      <td><?= $user['email']; ?></td>
+  				      <td><?= $user['name']; ?></td>
+  				      <td><?= $user['role']; ?></td>
+  				      <td><?= $user['is_active'] == 1 ? 'Yes' : 'No'; ?></td>
+  				      <td><?= date('d-M-Y H:m:s', $user['date_modified']);?></td>
+  				      <td>
+  				      		<a href="manageuser/<?= $user['id']; ?>" class="badge badge-success">edit</a>
+  				      		<a href="javascript:hapusData(<?= $user['id']; ?>)" class="badge badge-danger">delete</a>
+  				      </td>
+  				    </tr>
+  					<?php
+  						$i++;
+  						endforeach;
+  					?>
+  				  </tbody>
+				  </table>
 		  	</div>
         </div>
       </div>
